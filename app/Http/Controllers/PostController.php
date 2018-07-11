06d4e14f1355e53c8314ed32d053c47f245ad748
wangkezheng1234 <wangkezheng1234@163.com>
2018-07-11 16:59:20 +0800

    layout调整

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -7,14 +7,20 @@ use Illuminate\Http\Request;
 class PostController extends Controller
 {
     function index(){
-        return view("post/index");
+        $posts = [
+            ['title'=>'this is title1','author'=>'this is author1','content'=>'this is content1'],
+            ['title'=>'this is title2','author'=>'this is author2','content'=>'this is content2'],
+            ['title'=>'this is title3','author'=>'this is author3','content'=>'this is content3'],
+        ];
+        return view("post/index",array('posts'=>$posts));
     }
     function create(){
         return view("post/create");
     }
 
     function show(){
-        return view("post/show");
+        $data = array('a'=>'this is a','show'=>'false');
+        return view("post/show",$data);
     }
     function store(){
         
